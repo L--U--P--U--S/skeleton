60bdfc1d3380e37952937eff8c4dfe30f8affde9
Thomas Wolf <thomas.wolf@leibniz-hki.de>
2016-12-14 20:11:55 +0100

    cluster/cassis: Initial commit for test suite

---
+++ antismash/test/test_cassis.py
@@ -0,0 +1,29 @@
+# vim: set fileencoding=utf-8 :
+#
+# Copyright (C) 2016 Thomas Wolf
+# Leibniz Institute for Natural Product Research and
+# Infection Biology -- Hans-Knoell-Institute (HKI)
+#
+# License: GNU Affero General Public License v3 or later
+# A copy of GNU AGPL v3 should have been included in this software package in LICENSE.txt.
+
+"""Test suite for the cassis cluster detection plugin"""
+
+try:
+    import unittest2
+except ImportError:
+    import unittest as unittest2
+# import sys
+# import os
+# from os import path
+# from argparse import Namespace
+# from minimock import mock, restore, Mock, TraceTracker, assert_same_trace
+# from antismash import utils
+# from antismash import config
+# from Bio.SeqFeature import FeatureLocation
+# import subprocess
+from antismash.cluster import cassis
+
+class TestCassis(unittest2.TestCase):
+    pass
+    # print cassis._plus_minus
